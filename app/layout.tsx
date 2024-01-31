bf4e614d445950b574d1503271a185f7df04a5af
Vitor <contato@webfandoh.com>
2024-01-31 16:24:51 -0300

    add keywords in meta head

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,11 +20,10 @@ export default function RootLayout({
       <title>WebFandoh</title>
       <meta name="description" content="O WebFandoh é um lugar para explorar, criar e compartilhar com todo tipo de conteúdo. Uma plataforma onde você pode criar, descobrir e interagir com todos os tipos de conteúdo personalizados e interativos da internet."></meta>
       <meta name="application-name" content="WebFandoh"></meta>
-      <meta name="robots" content="index, follow"></meta>
+      <meta name="keywords" content="conteúdo interativo, criação de conteúdo, plataforma de compartilhamento, WebFandoh, comunidade online, conteúdo personalizado, experiências online, interação na internet, descobrir conteúdo, compartilhar conteúdo, explorar conteúdo, mídia interativa, entretenimento online, comunidade de criadores, diversão online, conteúdo da internet, participação online, criação de mídia, redes sociais, interatividade online, conteúdo criativo, WebFandoh comunidade, descoberta de conteúdo, plataforma de entretenimento, mídia online, compartilhamento criativo, diversidade de conteúdo, experiências digitais"></meta>
       <meta property="og:title" content="WebFandoh | Sua fonte de conteúdos divertidos!"></meta>
       <meta property="og:url" content="https://webfandoh.com/"></meta>
       <meta property="og:site_name" content="WebFandoh"></meta>
-
       </head>
       <body className={inter.className}>{children}</body>
     </html>
