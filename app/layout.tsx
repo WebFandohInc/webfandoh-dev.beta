4e7bf77183cfcf06f38299b00c1d3cf3d4e53a4a
Vitor <contato@webfandoh.com>
2024-02-02 14:53:52 -0300

    tetando por o analytics novamente

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,19 +17,33 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-      <title>WebFandoh</title>
-      <meta name="description" content="O WebFandoh é um lugar para explorar, criar e compartilhar com todo tipo de conteúdo. Uma plataforma onde você pode criar, descobrir e interagir com todos os tipos de conteúdo personalizados e interativos da internet."></meta>
-      <meta name="application-name" content="WebFandoh"></meta>
-      <meta name="robots" content="index, follow"></meta>      
-      <meta property="og:type" content="website"></meta>
-      <link rel="icon" href="/favicon.ico" sizes="any" />
-      <link rel="icon" href="/app/favicon.ico" />
-      <meta name="google-site-verification" content="9VJyIZjWIbiXrL0lfE75eWDvofIR-iOw5sC9eDE4Cls" />
-      <link rel="icon" href="https://webfandoh.com/favicon.ico" />
-      <meta name="keywords" content="conteúdo interativo, criação de conteúdo, plataforma de compartilhamento, WebFandoh, comunidade online, conteúdo personalizado, experiências online, interação na internet, descobrir conteúdo, compartilhar conteúdo, explorar conteúdo, mídia interativa, entretenimento online, comunidade de criadores, diversão online, conteúdo da internet, participação online, criação de mídia, redes sociais, interatividade online, conteúdo criativo, WebFandoh comunidade, descoberta de conteúdo, plataforma de entretenimento, mídia online, compartilhamento criativo, diversidade de conteúdo, experiências digitais"></meta>
-      <meta property="og:title" content="WebFandoh | Sua fonte de conteúdos divertidos!"></meta>
-      <meta property="og:url" content="https://webfandoh.com/"></meta>
-      <meta property="og:site_name" content="WebFandoh"></meta>
+        <title>WebFandoh</title>
+        <meta name="description" content="O WebFandoh é um lugar para explorar, criar e compartilhar com todo tipo de conteúdo. Uma plataforma onde você pode criar, descobrir e interagir com todos os tipos de conteúdo personalizados e interativos da internet."></meta>
+        <meta name="application-name" content="WebFandoh"></meta>
+        <meta name="robots" content="index, follow"></meta>   
+        <meta name="generator" content="WebFandoh"></meta>   
+        <meta property="og:type" content="website"></meta>
+        <meta name="google-adsense-account" content="ca-pub-3554757782177589" />
+        <meta property="og:site_name" content="WebFandoh"></meta>
+        <meta property="og:locale" content="pt-br"></meta>
+        <link rel="icon" href="/app/favicon.ico" />
+        <link rel="icon" href="https://webfandoh.com/favicon.ico" />
+        <meta name="keywords" content="conteúdo interativo, criação de conteúdo, plataforma de compartilhamento, WebFandoh, comunidade online, conteúdo personalizado, experiências online, interação na internet, descobrir conteúdo, compartilhar conteúdo, explorar conteúdo, mídia interativa, entretenimento online, comunidade de criadores, diversão online, conteúdo da internet, participação online, criação de mídia, redes sociais, interatividade online, conteúdo criativo, WebFandoh comunidade, descoberta de conteúdo, plataforma de entretenimento, mídia online, compartilhamento criativo, diversidade de conteúdo, experiências digitais"></meta>
+        <meta property="og:title" content="WebFandoh | Sua fonte de conteúdos divertidos!"></meta>
+        <meta property="og:url" content="https://webfandoh.com/"></meta>
+        <meta property="og:site_name" content="WebFandoh"></meta>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3554757782177589" crossOrigin="anonymous"></script>
+
+        {/* Script do Google Analytics */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-EK39T2EK57"></script>
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-EK39T2EK57');
+          `,
+        }} />
       </head>
       <body className={inter.className}>{children}</body>
     </html>
